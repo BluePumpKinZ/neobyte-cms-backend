ccb99429a12add6a1839397b88a55dcd6ad0043f
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-01-28 16:39:37 +0100

    Added TypeUtils

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Utils/Extensions/WebApplicationBuilderExtensions.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Utils/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Neobyte.Cms.Backend.Utils.Extensions; 
 
 public static class WebApplicationBuilderExtensions {
 
     public static WebApplicationBuilder AddUtils (this WebApplicationBuilder builder) {
-        return builder;
+		builder.Services.AddSingleton<TypeUtils>();
+		return builder;
     }
 
 }
