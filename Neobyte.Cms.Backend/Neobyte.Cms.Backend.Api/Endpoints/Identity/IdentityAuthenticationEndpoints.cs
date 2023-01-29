72e40843ee693d67a8fed88d35fee2b339c3ca77
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-01-29 21:02:58 +0100

    Added request validation

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Api/Endpoints/Identity/IdentityAuthenticationEndpoints.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Api/Endpoints/Identity/IdentityAuthenticationEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Neobyte.Cms.Backend.Api.Endpoints.Validation.Extensions;
 using Neobyte.Cms.Backend.Core.Identity.Managers;
 using Neobyte.Cms.Backend.Core.Identity.Models.Authentication;
 using System.Diagnostics;
@@ -21,7 +22,7 @@ public class IdentityAuthenticationEndpoints : IApiEndpoints {
 					IdentityRegisterResponseModel.RegisterResult.RequiresConfirmation => Results.Redirect("register/confirm"),
 					_ => throw new UnreachableException()
 				};
-			});
+			}).ValidateBody<IdentityRegisterRequestModel>();
 
 		routes.MapPost("login", async ([FromServices] IdentityAuthenticationManager manager,
 			[FromBody] IdentityLoginRequestModel request) => {
@@ -35,7 +36,7 @@ public class IdentityAuthenticationEndpoints : IApiEndpoints {
 					IdentityLoginResponseModel.LoginResult.Unknown => Results.BadRequest(),
 					_ => throw new UnreachableException()
 				};
-			});
+			}).ValidateBody<IdentityLoginRequestModel>();
 
 		routes.MapDelete("logout", async ([FromServices] IdentityAuthenticationManager manager) => {
 			await manager.Logout();
---
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Api/Endpoints/Validation/Extensions/RouteHandlerBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Neobyte.Cms.Backend.Api.Endpoints.Validation.Extensions; 
+
+public static class RouteHandlerBuilderExtensions {
+
+	public static RouteHandlerBuilder ValidateBody<T> (this RouteHandlerBuilder builder)
+		where T : class {
+		return builder.AddEndpointFilter<EndpointValidationFilter<T>>();
+	}
+
+}
