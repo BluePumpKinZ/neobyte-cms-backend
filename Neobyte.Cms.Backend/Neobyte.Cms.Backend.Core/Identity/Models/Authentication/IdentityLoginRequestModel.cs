449c4bc489f0f4a936cdeaee8151b97afaca53cb
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-01-30 19:09:10 +0100

    Added identitylogin requireds

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/Identity/Models/Authentication/IdentityLoginRequestModel.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/Identity/Models/Authentication/IdentityLoginRequestModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Neobyte.Cms.Backend.Core.Identity.Models.Authentication;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Neobyte.Cms.Backend.Core.Identity.Models.Authentication;
 
 public class IdentityLoginRequestModel {
 
+	[Required]
 	public string Email { get; set; } = string.Empty;
+	[Required]
 	public string Password { get; set; } = string.Empty;
 	public bool RememberMe { get; set; }
 
