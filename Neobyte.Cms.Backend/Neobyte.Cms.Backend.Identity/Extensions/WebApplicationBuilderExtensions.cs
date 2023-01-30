8ab63f60f2e3fb006ea31b40a06421e19e1b1440
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-01-30 19:28:50 +0100

    Removed aspnet identity

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Identity/Extensions/WebApplicationBuilderExtensions.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Identity/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,12 +1,8 @@
-﻿using System.Linq;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Neobyte.Cms.Backend.Core.Ports.Identity;
-using Neobyte.Cms.Backend.Domain.Accounts;
 using Neobyte.Cms.Backend.Identity.Adapters;
-using Neobyte.Cms.Backend.Identity.Policies;
-using Neobyte.Cms.Backend.Persistence.EF;
 
 namespace Neobyte.Cms.Backend.Identity.Extensions;
 
@@ -18,31 +14,9 @@ public static class WebApplicationBuilderExtensions {
 		builder.Services.AddScoped<IIdentityAuthorizationProvider, IdentityAuthorizationProvider>();
 		builder.Services.AddScoped<IIdentityRoleProvider, IdentityRoleProvider>();
 
-		builder.Services.AddIdentity<AccountIdentityUser, IdentityRole>()
-			.AddEntityFrameworkStores<EFDbContext>()
-			.AddDefaultTokenProviders();
-
 		builder.Services.Configure<IdentityOptions>(builder.Configuration.GetSection("Identity"));
 
-		builder.Services.ConfigureApplicationCookie(opt => {
-			opt.LoginPath = "/login";
-			opt.AccessDeniedPath = "/access-denied";
-		});
-
-		builder.Services.AddAuthorization(opt => {
-			foreach (UserPolicy userPolicy in UserPolicyData.GetValues()) {
-				opt.AddPolicy(userPolicy.GetPolicyName(), policy => {
-					policy.RequireAuthenticatedUser();
-					policy.RequireRole(userPolicy.GetPolicyRoles().Select(userRole => (string)userRole));
-				});
-			}
-		});
-
-		builder.Services.AddAuthentication();
-
 		return builder;
-
-
 	}
 
 }
