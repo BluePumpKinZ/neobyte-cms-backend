2219a869873bd95dc88b9b12f3fdd48d24edd5d0
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-02-12 12:57:20 +0100

    Added deleting of accounts

---
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/Exceptions/Persistence/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Neobyte.Cms.Backend.Core.Exceptions.Persistence;
+
+public abstract class NotFoundException : ApplicationException {
+
+	public NotFoundException () {}
+
+	public NotFoundException (string? message) : base (message) {}
+
+	public NotFoundException (string? message, Exception? innerException) : base (message, innerException) {}
+
+}
