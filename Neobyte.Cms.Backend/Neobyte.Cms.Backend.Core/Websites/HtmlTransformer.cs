a26c52e4f4cd4b78b6d7abac7275e08b7706af62
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-02-15 20:33:40 +0100

    Added contenteditable=true to cms editable html elements

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/Websites/HtmlTransformer.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/Websites/HtmlTransformer.cs
@@ -5,13 +5,18 @@ namespace Neobyte.Cms.Backend.Core.Websites;
 public class HtmlTransformer {
 
 	private readonly Regex _htmlHeadRegex = new Regex ("(.*<head>)()([\\S\\s]*)(<\\/head>.*)", RegexOptions.Compiled);
+	private readonly Regex _htmlCmsEditableRegex = new Regex("(<.* class=\".*cms-editable.*\")()(.*>)", RegexOptions.Compiled);
 	
 	private string GenerateBaseHrefTag(string domain) {
 		return $"<base href=\"{domain}/\">";
 	}
 
 	public string TransformRenderedWebpage (string domain, string htmlContent) {
-		return _htmlHeadRegex.Replace(htmlContent, m => m.Groups[1] + GenerateBaseHrefTag(domain) + m.Groups[3] + m.Groups[5]);
+		htmlContent = _htmlHeadRegex.Replace(htmlContent, m => m.Groups[1] + GenerateBaseHrefTag(domain) + m.Groups[3] + m.Groups[5]);
+
+		htmlContent = htmlContent.Replace("cms-editable", "cms-editable mce-content-body cms-initialized");
+
+		return _htmlCmsEditableRegex.Replace(htmlContent, m => m.Groups[1] + " contenteditable=\"true\" " + m.Groups[3]);
 	}
 
 }
