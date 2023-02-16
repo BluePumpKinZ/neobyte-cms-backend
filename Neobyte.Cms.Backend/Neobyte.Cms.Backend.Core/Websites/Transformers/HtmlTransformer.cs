101a15d445839bec370c6abd9d2103c93db8861b
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-02-16 18:03:17 +0100

    Added rendered publishing

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/Websites/Transformers/HtmlTransformer.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/Websites/Transformers/HtmlTransformer.cs
@@ -10,6 +10,9 @@ public partial class HtmlTransformer {
 	private static readonly Regex HTMLHeadRegex = GetHtmlHeadRegex();
 	private static readonly Regex HTMLCmsEditableRegex = GetHtmlCmsEditableRegex();
 
+	private static readonly Regex HTMLBaseHrefRegex = GetHtmlBaseHrefRegex();
+	private static readonly Regex CssStylingRegex = GetCssStylingRegex();
+
 	private readonly HtmlTransformerOptions _options;
 	private readonly ILogger<HtmlTransformer> _logger;
 	private string _cssStyles = "";
@@ -29,21 +32,36 @@ public partial class HtmlTransformer {
 		_cssStyles = File.ReadAllText(_options.CmsStylesPath);
 	}
 
-	private static string GenerateBaseHrefTag (string domain) {
-		return $"<base href=\"{domain}/\">";
-	}
-
-	public string TransformRenderedWebpage (string domain, string htmlContent) {
-		htmlContent = HTMLHeadRegex.Replace(htmlContent, m => m.Groups[1] + GenerateBaseHrefTag(domain)
-		+ m.Groups[3] + _cssStyles + m.Groups[5])
-			.Replace("cms-editable", "cms-editable mce-content-body cms-initialized");
-
-		return HTMLCmsEditableRegex.Replace(htmlContent, m => m.Groups[1] + " contenteditable=\"true\" " + m.Groups[3]);
-	}
-
+	// Construction
 	[GeneratedRegex("(.*<head>)()([\\S\\s]*)(<\\/head>.*)", RegexOptions.Compiled)]
 	private static partial Regex GetHtmlHeadRegex ();
 	[GeneratedRegex("(<.* class=\".*cms-editable.*\")()(.*>)", RegexOptions.Compiled)]
 	private static partial Regex GetHtmlCmsEditableRegex ();
+
+	private static string GenerateBaseHrefTag (string domain) {
+		return $"<base href=\"{domain}/\">";
+	}
+
+	public string ConstructRenderedWebpage (string domain, string htmlContent) {
+		htmlContent = HTMLHeadRegex.Replace(htmlContent, m => m.Groups[1] + GenerateBaseHrefTag(domain)
+		+ m.Groups[3] + _cssStyles + m.Groups[5])
+			.Replace("cms-editable", "cms-editable mce-content-body cms-initialized");
+
+		return HTMLCmsEditableRegex.Replace(htmlContent, m => m.Groups[1] + " contenteditable=\"true\"" + m.Groups[3]);
+	}
+
+	// Deconstruction
+	[GeneratedRegex("<base href=\".*\">", RegexOptions.Compiled)]
+	private static partial Regex GetHtmlBaseHrefRegex ();
+	[GeneratedRegex("<style id=\"cms_style_k4U\".*>[\\S\\s]*<\\/style>", RegexOptions.Compiled)]
+	private static partial Regex GetCssStylingRegex ();
+
+	public string DeconstructRenderedWebPage (string htmlContent) {
+		htmlContent = htmlContent.Replace("cms-editable mce-content-body cms-initialized", "cms-editable")
+			.Replace("contenteditable=\"true\"", "");
+		htmlContent = HTMLBaseHrefRegex.Replace(htmlContent, "");
+		htmlContent = CssStylingRegex.Replace(htmlContent, "");
+		return htmlContent;
+	}
 
 }
