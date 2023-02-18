a22d30804cee9f7ec825f87d9688b80c1e484d1a
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-02-18 20:56:45 +0100

    Reenabled remote connection closed

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.RemoteHosting/Extensions/WebApplicationExtensions.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.RemoteHosting/Extensions/WebApplicationExtensions.cs
@@ -8,7 +8,7 @@ public static class WebApplicationExtensions {
 
 	public static WebApplication UseRemoteHosting (this WebApplication app) {
 		var disconnector = app.Services.GetRequiredService<ConnectionDisconnector>();
-		// disconnector.Start();
+		disconnector.Start();
 		return app;
 	}
 
