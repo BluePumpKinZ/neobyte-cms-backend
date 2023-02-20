176df0c7fda46702fe92f23fe0f1abe88cf6c826
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-02-20 16:19:29 +0100

    Removed TracedAttribute

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/RemoteHosting/IRemoteHostingConnector.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Core/RemoteHosting/IRemoteHostingConnector.cs
@@ -1,5 +1,4 @@
-﻿using Neobyte.Cms.Backend.Core.Monitoring;
-using Neobyte.Cms.Backend.Domain.Websites.HostingConnections;
+﻿using Neobyte.Cms.Backend.Domain.Websites.HostingConnections;
 using System;
 
 namespace Neobyte.Cms.Backend.Core.RemoteHosting;
@@ -12,34 +11,24 @@ public interface IRemoteHostingConnector {
 
 	public void Configure (HostingConnection connection);
 
-	[Traced]
 	public Task<IEnumerable<FilesystemEntry>> ListItemsAsync (string path);
 
-	[Traced]
 	public Task CreateFolderAsync (string path);
 
-	[Traced]
 	public Task RenameFolderAsync (string path, string newPath);
 
-	[Traced]
 	public Task DeleteFolderAsync (string path);
 
-	[Traced]
 	public Task CreateFileAsync (string path, byte[] content);
 
-	[Traced]
 	public Task RenameFileAsync (string path, string newPath);
 
-	[Traced]
 	public Task DeleteFileAsync (string path);
 
-	[Traced]
 	public Task<byte[]> GetFileContentAsync (string path);
 
-	[Traced]
 	public Task<bool> FolderExistsAsync (string path);
 
-	[Traced]
 	public Task<bool> FileExistsAsync (string path);
 
 	public void Dispose ();
