56d1167179cad8c256297a0221ddd0205b23d6ed
coolsarne <coolsarne@hotmail.be>
2023-02-23 16:39:47 +0100

    migration for WebsiteAccounts table

---
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Persistence/Migrations/20230223152353_WebsiteAccounts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Neobyte.Cms.Backend.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class WebsiteAccounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebsiteAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WebsiteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebsiteAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WebsiteAccounts_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WebsiteAccounts_Websites_WebsiteId",
+                        column: x => x.WebsiteId,
+                        principalTable: "Websites",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebsiteAccounts_AccountId",
+                table: "WebsiteAccounts",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WebsiteAccounts_WebsiteId",
+                table: "WebsiteAccounts",
+                column: "WebsiteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebsiteAccounts");
+        }
+    }
+}
