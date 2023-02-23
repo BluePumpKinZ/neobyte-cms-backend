6ec19025a0e71d841460dcff240397e5eb5f11f1
coolsarne <coolsarne@hotmail.be>
2023-02-23 01:09:38 +0100

    api preps

---
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Api/Endpoints/Websites/WebsiteAccountEndpoints.cs
@@ -0,0 +1,23 @@
+﻿using Neobyte.Cms.Backend.Core.Websites.Managers;
+using Neobyte.Cms.Backend.Domain.Websites;
+
+namespace Neobyte.Cms.Backend.Api.Endpoints.Websites; 
+
+internal class WebsiteUserEndpoints : IApiEndpoints {
+	
+	public string GroupName => "Website Users";
+	public string Path => "/api/v1/websites/{websiteId:Guid}/users";
+	public bool Authorized => true;
+	
+	public void RegisterApis (RouteGroupBuilder routes) {
+		
+		routes.MapGet("all", async (
+				[FromServices] WebsiteAccountManager manager,
+				[FromServices] Projector projector,
+				[FromRoute] Guid websiteId) => {
+					var websiteAccounts = await manager.GetAccountsByWebsiteIdAsync(new WebsiteId(websiteId));
+					// var projection = projector.Project<WebsiteAccount, WebsiteAccountProjection>(websiteAccounts);
+					return Results.Ok();
+				}).Authorize(UserPolicy.ClientPrivilege);
+	}
+}
