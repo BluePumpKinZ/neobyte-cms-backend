f11e280c192330c371e6aea80e3da01ed6d4c90d
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-02-24 20:17:53 +0100

    Added pathutils tests

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Utils/PathUtils.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Utils/PathUtils.cs
@@ -1,17 +1,34 @@
-﻿using System.Linq;
+﻿using System.Diagnostics.Contracts;
+using System.Linq;
 
-namespace Neobyte.Cms.Backend.Utils; 
+namespace Neobyte.Cms.Backend.Utils;
 
 public class PathUtils {
 
+	[Pure]
 	public string Combine (params string[] paths) {
-		return '/' + paths.Select(path => path.Trim().Trim('/'))
-			.Aggregate((a, b) => string.Join('/', a, b));
+		if (paths.Length == 0)
+			return "";
+		var parts = paths.Select(path => path.Trim().Trim('/'))
+		.Where(s => !string.IsNullOrWhiteSpace(s));
+		var joined = string.Join('/', parts);
+		return CollapseSlashes ('/' + joined);
 	}
 
+	[Pure]
 	public string GetPathAbove (string path) {
+		path = CollapseSlashes(path);
+		path = path.Trim().Trim('/');
 		var parts = path.Split('/');
+		if (parts.Length == 0)
+			return "";
 		return Combine(parts.Take(parts.Length - 1).ToArray());
+	}
+
+	[Pure]
+	private string CollapseSlashes (string path) {
+		// remove duplicate slashes
+		return path.Replace("//", "/");
 	}
 
 }
