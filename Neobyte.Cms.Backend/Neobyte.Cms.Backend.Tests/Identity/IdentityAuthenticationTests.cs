f315ec5b10c3866eed47e0d35cefc2d2f4192b16
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-02-25 13:10:44 +0100

    Added basic api testing endpoint

---
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Tests/Identity/IdentityAuthenticationTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Neobyte.Cms.Backend.Tests.Identity;
+
+public class IdentityAuthenticationTests {
+
+	private readonly WebApplicationFactory<Program> _factory;
+
+	public IdentityAuthenticationTests () {
+		_factory = new WebApplicationFactory<Program>();
+	}
+
+	[Fact]
+	public async Task Login_ShouldReturnJwtToken_IfCredentialsAreValid () {
+
+		// Arrange
+		var client = _factory.CreateClient();
+
+		// Act
+		var response = await client.PostAsJsonAsync("/api/v1/identity/authentication/login", new {
+			Email = "admin@neobyte.net",
+			Password = "Ne0byteCMS!",
+			RememberMe = false
+		});
+
+		// Assert
+		response.EnsureSuccessStatusCode();
+		Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType!.ToString());
+
+		var r = new { Token = "", Expires = 0L };
+
+		dynamic? result = response.Content.ReadFromJsonAsync(r.GetType()).Result;
+		Assert.NotNull(result);
+		Assert.NotNull(result!.Token);
+		Assert.NotNull(result.Expires);
+	}
+
+}
