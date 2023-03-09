cb4ef3820c58b432c49718c9f24c3b15de30f526
Jonas Leijzen <jonas.leijzen@student.kdg.be>
2023-03-10 00:48:21 +0100

    Added website thumbnail to website projection

--- Neobyte.Cms.Backend/Neobyte.Cms.Backend.Api/Projections/Projections/WebsiteProjection.cs
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Api/Projections/Projections/WebsiteProjection.cs
@@ -8,6 +8,7 @@ internal class WebsiteProjection : IProjection {
 	public WebsiteId Id { get; set; }
 	public string Name { get; set; } = string.Empty;
 	public string Domain { get; set; } = string.Empty;
+	public string Screenshot => $"http://localhost:5110/api/v1/websites/{Id}/thumbnail";
 	public DateTime CreatedDate { get; set; }
 
 	public void RegisterMap (IMapperConfigurationExpression configuration) {
