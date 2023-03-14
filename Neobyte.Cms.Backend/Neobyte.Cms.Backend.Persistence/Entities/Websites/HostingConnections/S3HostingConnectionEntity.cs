ec08e4218dd4511b80d3fccaedd2b04516f3021a
coolsarne <coolsarne@hotmail.be>
2023-03-15 00:26:26 +0100

    implemented entity, TODO migrations

---
+++ Neobyte.Cms.Backend/Neobyte.Cms.Backend.Persistence/Entities/Websites/HostingConnections/S3HostingConnectionEntity.cs
@@ -0,0 +1,11 @@
+﻿using Neobyte.Cms.Backend.Domain.Websites.HostingConnections;
+
+namespace Neobyte.Cms.Backend.Persistence.Entities.Websites.HostingConnections; 
+
+public class S3HostingConnectionEntity : HostingConnectionEntity{
+	
+	
+	
+	public S3HostingConnectionEntity(HostingConnectionId id) : base(id){
+	}
+}
